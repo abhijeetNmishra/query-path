27addfdea96a18d30da0506e0afb49caa7800966
Abhijeet.N.Mishra <Abhijeet.N.Mishra@target.com>
2015-11-28 04:11:05 -0600

    added pathByIndex and pathByMatchingString

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 'use strict';
-exports.qsval = function(name, url) {
-  name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
-  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)");
+var _ = require('lodash');
+exports.queryStringByKey = function(key, url) {
+  key = key.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+  var regex = new RegExp("[\\?&]" + key + "=([^&#]*)");
   if (url) {
     var results = regex.exec(url);
   } else {
@@ -9,3 +10,38 @@ exports.qsval = function(name, url) {
   }
   return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
+
+exports.pathByIndex = function(index, url) {
+  var pathname = '';
+  var retVal = '';
+  if (url) {
+    pathname = url;
+  } else {
+    pathname = window.location.pathname;
+  }
+  pathname = _.compact(pathname.split('/'));
+  if (index < pathname.length) {
+    retVal = pathname[index];
+  }
+  return retVal;
+}
+
+exports.pathByMatchingString = function(matchingString, url) {
+    var pathname = '';
+    var retVal = [];
+    if (url) {
+      pathname = url;
+    } else {
+      pathname = window.location.pathname;
+    }
+    pathname = _.compact(pathname.split('/'));
+    if (matchingString && !_.isEmpty(pathname) {
+        _.each(pathname, function(val, i) {
+          if (val.indexOf(matchingString) >= 0) {
+            retVal.push[val];
+          }
+        });
+      }
+
+      return retVal;
+    }
